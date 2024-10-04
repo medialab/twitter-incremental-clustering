6ad57f12504dfa5c7be9a862f2623c6c91c78c0e
bmaz <beatrice.mazoyer@gmail.com>
2024-10-04 11:42:36 +0200

    Switch to ruff code formatting

--- generate_table.py
+++ generate_table.py
@@ -12,30 +12,38 @@ df["model"] = df.model.str.replace("HISEvent", "HE")
 df["model"] = df.model.str.replace("twembeddings", "TW")
 df["dataset"] = df.dataset.str.replace(".tsv", "")
 
-idx = df.groupby(['dataset', 'model'])['ARI'].transform('max') == df["ARI"]
+idx = df.groupby(["dataset", "model"])["ARI"].transform("max") == df["ARI"]
 
 ari = df[idx].pivot(index="dataset", columns="model", values="ARI")
 ari["metric"] = "ARI"
 ami = df[idx].pivot(index="dataset", columns="model", values="AMI")
 ami["metric"] = "AMI"
 
-table = pd.concat([ari, ami]).reset_index().sort_values("dataset").set_index(["dataset", "metric"])
+table = (
+    pd.concat([ari, ami])
+    .reset_index()
+    .sort_values("dataset")
+    .set_index(["dataset", "metric"])
+)
 
 styler = table.style
 styler.format(precision=2)
 styler.format_index("\\textbf{{{}}}", escape="latex", axis=0)
 styler.format_index("\\textbf{{{}}}", escape="latex", axis=1)
 
-def highlight_max(s, props=''):
-    return np.where(s == np.nanmax(s.values), props, '')
-styler.apply(highlight_max, props='font-weight: bold', axis=1)
+
+def highlight_max(s, props=""):
+    return np.where(s == np.nanmax(s.values), props, "")
+
+
+styler.apply(highlight_max, props="font-weight: bold", axis=1)
 
 table_str = styler.to_latex(
     multirow_align="c",
     hrules=True,
-    column_format="ll" + "c"*(table.shape[1]),
-    convert_css=True
-    )
+    column_format="ll" + "c" * (table.shape[1]),
+    convert_css=True,
+)
 
 filename = METRICS_FILE.replace(".csv", ".tex")
 print("Write latex table to {}".format(filename))
--- preprocess.py
+++ preprocess.py
@@ -3,31 +3,59 @@ import numpy as np
 import csv
 
 
-#Load Event2018
-columns = ["tweet_id", "user_name", "text", "time", "event_id", "user_mentions", \
-    "hashtags", "urls", "words", "created_at", "filtered_words", "entities", "sampled_words"]
-df_np = np.load('raw_data/Event2018/french_tweets.npy', allow_pickle=True)
+# Load Event2018
+columns = [
+    "tweet_id",
+    "user_name",
+    "text",
+    "time",
+    "event_id",
+    "user_mentions",
+    "hashtags",
+    "urls",
+    "words",
+    "created_at",
+    "filtered_words",
+    "entities",
+    "sampled_words",
+]
+df_np = np.load("raw_data/Event2018/french_tweets.npy", allow_pickle=True)
 df_2018 = pd.DataFrame(data=df_np, columns=columns)
 
-#Load Event2012
-p_part1 = 'raw_data/Event2012/68841_tweets_multiclasses_filtered_0722_part1.npy'
-p_part2 = 'raw_data/Event2012/68841_tweets_multiclasses_filtered_0722_part2.npy'
-columns=["event_id", "tweet_id", "text", "user_id", "created_at", "user_loc",\
-    "place_type", "place_full_name", "place_country_code", "hashtags", "user_mentions", \
-    "image_urls", "entities", "words", "filtered_words", "sampled_words"]
+# Load Event2012
+p_part1 = "raw_data/Event2012/68841_tweets_multiclasses_filtered_0722_part1.npy"
+p_part2 = "raw_data/Event2012/68841_tweets_multiclasses_filtered_0722_part2.npy"
+columns = [
+    "event_id",
+    "tweet_id",
+    "text",
+    "user_id",
+    "created_at",
+    "user_loc",
+    "place_type",
+    "place_full_name",
+    "place_country_code",
+    "hashtags",
+    "user_mentions",
+    "image_urls",
+    "entities",
+    "words",
+    "filtered_words",
+    "sampled_words",
+]
 df_np_part1 = np.load(p_part1, allow_pickle=True)
 df_np_part2 = np.load(p_part2, allow_pickle=True)
-df_np = np.concatenate((df_np_part1, df_np_part2), axis = 0)
+df_np = np.concatenate((df_np_part1, df_np_part2), axis=0)
 df_2012 = pd.DataFrame(data=df_np, columns=columns)
 
-#Write csv version of Event2018
-df_2018[["tweet_id", "text", "created_at", "event_id"]]\
-    .rename(columns={"tweet_id": "id", "event_id": "label"})\
-    .to_csv("event2018.tsv", sep="\t", quoting=csv.QUOTE_ALL, index=False)
+# Write csv version of Event2018
+df_2018[["tweet_id", "text", "created_at", "event_id"]].rename(
+    columns={"tweet_id": "id", "event_id": "label"}
+).to_csv("event2018.tsv", sep="\t", quoting=csv.QUOTE_ALL, index=False)
 
-#Write csv version of Event2012
-df_2012[["tweet_id", "text", "created_at", "event_id"]]\
-    .rename(columns={"tweet_id": "id", "event_id": "label"})\
-    .to_csv("event2012.tsv", sep="\t", quoting=csv.QUOTE_ALL, index=False)
+# Write csv version of Event2012
+df_2012[["tweet_id", "text", "created_at", "event_id"]].rename(
+    columns={"tweet_id": "id", "event_id": "label"}
+).to_csv("event2012.tsv", sep="\t", quoting=csv.QUOTE_ALL, index=False)
 
 print("Save preprocessed files to event2012.tsv and event2018.tsv")
